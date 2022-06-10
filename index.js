801e2a61b249a6ea439ae7d80738cbdc74fb552d
taibislamdipu <mailtaibislam@gmail.com>
2022-06-10 08:23:59 +0600

    new

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -34,7 +35,7 @@ async function run() {
       console.log(`A document was inserted with the _id: ${result.insertedId}`);
       console.log("added new product", req.body);
       console.log("added product", result);
-      res.send(result);
+      res.json(result);
     });
 
     //GET API
@@ -43,12 +44,21 @@ async function run() {
       const query = {};
       const cursor = await productCollection.find(query);
       const products = await cursor.toArray();
-      res.send(products);
+      res.json(products);
 
       // print a message if no documents were found
       if ((await cursor.count()) === 0) {
         console.log("No documents found!");
       }
+    });
+
+    // DELETE ONE ITEM API
+    app.delete("/products/:id", async function (req, res) {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.json(result);
+      console.log(result);
     });
   } finally {
     // await client.close();
